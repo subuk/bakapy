9018c2f1b662f447f3471aaa9c71578147c63751
Matvey Kruglov <kubuzzzz@gmail.com>
2014-09-28 19:16:32 +0400

    Move common functions to bakapy module

--- src/bakapy-scheduler/main.go
+++ src/bakapy-scheduler/main.go
@@ -7,51 +7,15 @@ import (
 	"github.com/op/go-logging"
 	"github.com/robfig/cron"
 	"os"
-	"path"
-	"strings"
 )
 
 var logger = logging.MustGetLogger("bakapy.scheduler")
 var CONFIG_PATH = flag.String("config", "", "Path to config file")
 var LOG_LEVEL = flag.String("loglevel", "info", "Log level")
 
-func setupLogging() error {
-	format := "%{color}%{time:15:04:05} %{level:.8s} %{module} %{message}%{color:reset}"
-	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
-	logging.SetBackend(logBackend)
-	logging.SetFormatter(logging.MustStringFormatter(format))
-	level, err := logging.LogLevel(strings.ToUpper(*LOG_LEVEL))
-	if err != nil {
-		return err
-	}
-	logging.SetLevel(level, "")
-	return nil
-}
-
-func makeJobRunner(job *bakapy.Job, config *bakapy.Config) func() {
-	return func() {
-		if job.IsDisabled() {
-			logger.Warning("job %s disabled, skipping", job.Name)
-			return
-		}
-		metadata := job.Run()
-		saveTo := path.Join(config.MetadataDir, string(metadata.TaskId))
-		err := metadata.Save(saveTo)
-		if err != nil {
-			logger.Critical("cannot save metadata: %s", err)
-		}
-		logger.Info("metadata for job %s successfully saved to %s", metadata.TaskId, saveTo)
-		if !metadata.Success {
-			logger.Critical("job '%s' failed", job.Name)
-		} else {
-			logger.Info("job '%s' finished", job.Name)
-		}
-	}
-}
-
 func main() {
 	flag.Parse()
-	err := setupLogging()
+	err := bakapy.SetupLogging(*LOG_LEVEL)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -74,14 +38,13 @@ func main() {
 			jobName, jobConfig,
 			storage.JobsChan, config,
 		)
-		scheduler.AddFunc(runSpec, makeJobRunner(job, config))
+		scheduler.AddFunc(runSpec, func() {
+			bakapy.RunJob(job, config, logger)
+		})
 	}
 
 	storage.Start()
 	scheduler.Start()
 
-	c := make(chan struct{})
-	for {
-		<-c
-	}
+	select {}
 }
---
+++ src/bakapy/utils.go
@@ -0,0 +1,40 @@
+package bakapy
+
+import (
+	"github.com/op/go-logging"
+	"os"
+	"path"
+	"strings"
+)
+
+func SetupLogging(logLevel string) error {
+	format := "%{color}%{time:15:04:05} %{level:.8s} %{module} %{message}%{color:reset}"
+	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
+	logging.SetBackend(logBackend)
+	logging.SetFormatter(logging.MustStringFormatter(format))
+	level, err := logging.LogLevel(strings.ToUpper(logLevel))
+	if err != nil {
+		return err
+	}
+	logging.SetLevel(level, "")
+	return nil
+}
+
+func RunJob(job *Job, config *Config, logger *logging.Logger) {
+	if job.IsDisabled() {
+		logger.Warning("job %s disabled, skipping", job.Name)
+		return
+	}
+	metadata := job.Run()
+	saveTo := path.Join(config.MetadataDir, string(metadata.TaskId))
+	err := metadata.Save(saveTo)
+	if err != nil {
+		logger.Critical("cannot save metadata: %s", err)
+	}
+	logger.Info("metadata for job %s successfully saved to %s", metadata.TaskId, saveTo)
+	if !metadata.Success {
+		logger.Critical("job '%s' failed", job.Name)
+	} else {
+		logger.Info("job '%s' finished", job.Name)
+	}
+}
