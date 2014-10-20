31bbb1346d3cb8f199472af688e09a2aa17bda72
Matvey Kruglov <kubuzzzz@gmail.com>
2014-10-20 16:36:52 +0400

    Add Content-Type header to email messages

--- src/bakapy/const.go
+++ src/bakapy/const.go
@@ -26,6 +26,7 @@ const JOB_FINISH = "_@!_JOB_FINISH_!@_"
 var MAIL_TEMPLATE_JOB_FAILED = template.Must(template.New("mail").Parse(`From: {{ .From }}
 To: {{.To}}
 Subject: {{.Subject}}
+Content-Type: text/plain;charset=utf8
 
 Job {{.JobName}} failed:
 {{.Message}}
