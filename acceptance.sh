0a48d26eccd3c1aad7856e9d7769cf1fe36343fd
Matvey Kruglov <kubuzzzz@gmail.com>
2015-02-15 15:00:09 +0300

    Add -v flag to acceptance.sh

--- acceptance.sh
+++ acceptance.sh
@@ -3,17 +3,21 @@
 for runTest in $(find acceptance/ -name 'run.sh');do
     pushd $(dirname $runTest) >/dev/null
     test -x clean.sh && ./clean.sh
-    testOutput=$(bash run.sh 2>&1)
-    testRet=$?
+
+    if [ "$1" == "-v" ];then
+        testOutput=""
+        ./run.sh
+        testRet=$?
+    else
+        testOutput=$(bash run.sh 2>&1)
+        testRet=$?
+    fi
+
     test -x clean.sh && ./clean.sh
     if [ "$testRet" != 0 ];then
         echo "fail	$(dirname $runTest). See output below."
         echo -e "$testOutput"
         exit 1
-    else
-        if [ "$1" == "-v" ];then
-            echo -e "$testOutput"
-        fi
     fi
     popd >/dev/null
     echo "ok	$(dirname $runTest)"
